a4af40a0a8750d569c478cffbe35849c00a3cd78
shivangi2002 <shivangi.gautam200211@gmail.com>
2026-04-08 21:51:42 +0530

    added forward function

--- linear.py
+++ linear.py
@@ -5,3 +5,12 @@ class Linear:
     def __init__(self, input_size):
         self.weights = [random() for _ in range(input_size)]
         self.bias = 0
+        self.input_size = input_size
+
+    def forward(self, inputs):
+        self.inputs = inputs
+        output = self.bias
+        for w,i in zip(self.weights, inputs):
+            output += i * w
+        return output
+    
