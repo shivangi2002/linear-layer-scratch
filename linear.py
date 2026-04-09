db6986ba9a895049f7adefa747b801b5030528ce
shivangi2002 <shivangi.gautam200211@gmail.com>
2026-04-09 22:48:58 +0530

    created backward function

--- linear.py
+++ linear.py
@@ -2,15 +2,26 @@ from random import random
 
 
 class Linear:
-    def __init__(self, input_size):
-        self.weights = [random() for _ in range(input_size)]
+    def __init__(self, inputs):
+        self.input_size = len(inputs)
+        self.weights = [random() for _ in range(self.input_size)]
         self.bias = 0
-        self.input_size = input_size
-
-    def forward(self, inputs):
+        
         self.inputs = inputs
+        
+
+    def forward(self):
         output = self.bias
-        for w,i in zip(self.weights, inputs):
+        for w,i in zip(self.weights, self.inputs):
+           
             output += i * w
         return output
     
+    def backward(self, target,lr):
+        output = self.forward()
+        error = output - target
+        self.bias -= error * lr
+    
+        for i in range(self.input_size):
+            self.weights[i] -= error * self.inputs[i] * lr
+            
